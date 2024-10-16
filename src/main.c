ac53ad777013e2ede4d03a3ed04b5fcaf669a52d
chanma <yunosuke.homma@icloud.com>
2024-10-17 01:32:54 +0900

    [update]parse_options関数を追加

--- src/main.c
+++ src/main.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-#include <string.h>
-
-#include "error.h"
+#include "options.h"
+#include "ft_ssl.h"
 
 int main(int ac, char **av) {
-  (void)ac;
-  (void)av;
   if (ac < 2) {
     print_err(ERR_NO_ARGUMENTS, NULL);
     return 1;
@@ -13,6 +10,15 @@ int main(int ac, char **av) {
     print_err(ERR_INVALID_COMMAND, av[1]);
     return 1;
   }
+  
+  // optionがついてるか、ついてるなら処理
+  t_options opts;
+  if (parse_options(ac, av, &opts)) {
+    return 1;
+  }
+
+  // 標準入力から値を受け取る処理
+  
   printf("Hello, World!\n");
   return 0;
 }
