ffa0518966ece2f98a2d2231ac32eb08edf18d46
chanma <yunosuke.homma@icloud.com>
2024-10-16 21:14:25 +0900

    [update]指定したコマンドのみ入力できるように

--- src/main.c
+++ src/main.c
@@ -7,10 +7,10 @@ int main(int ac, char **av) {
   (void)ac;
   (void)av;
   if (ac < 2) {
-    print_err(ERR_NO_ARGUMENTS);
+    print_err(ERR_NO_ARGUMENTS, NULL);
     return 1;
-  } else if (strcmp(av[1], "md5") || strcmp(av[1], "sha256")) {
-    print_err(ERR_INVALID_COMMAND);
+  } else if (strcmp(av[1], "md5") != 0 && strcmp(av[1], "sha256") != 0) {
+    print_err(ERR_INVALID_COMMAND, av[1]);
     return 1;
   }
   printf("Hello, World!\n");
