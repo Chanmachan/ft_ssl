a2f0a3a83a0528ce145dd1757030c96fa27bb734
chanma <yunosuke.homma@icloud.com>
2024-10-17 01:39:35 +0900

    [add]オプションをチェックする関数を追加

---
+++ src/options.c
@@ -0,0 +1,35 @@
+#include "options.h"
+#include <unistd.h>
+#include <string.h>
+
+bool parse_options(int ac, char **av, t_options *opts) {
+  for (int i = 2; i < ac; i++) {
+    if (av[i][0] == '-'){
+      if (strlen(av[i]) == 2) {
+	switch (av[i][1]) {
+	case 'p':
+	  opts->p = true;
+	  break;
+	case 'q':
+	    opts->q = true;
+	  break;
+	case 'r':
+	  opts->r = true;
+	  break;
+	case 's':
+	  opts->s = true;
+	  break;
+	default:
+	  print_err(ERR_INVALID_OPTION, av[i]);
+	  return 1;
+	}
+      } else {
+	print_err(ERR_INVALID_OPTION, av[i]);
+	return 1;
+      }
+    } else {
+      break;
+    }
+  }
+  return 0;
+}
