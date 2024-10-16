ec27095d40065c1f3518e94007b7eea58d845d65
chanma <yunosuke.homma@icloud.com>
2024-10-16 21:13:49 +0900

    [update]エラーメッセージを動的に表示できるように

--- include/error.h
+++ include/error.h
@@ -2,6 +2,12 @@
 #define ERROR_H
 
 #include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+// 84 characters fixed length
+// 83+nul
+#define DEFAULT_MSG_LEN 84
 
 typedef enum e_error_code{
   ERR_NO_ARGUMENTS,
@@ -10,11 +16,18 @@ typedef enum e_error_code{
 
 static const char* ERROR_MESSAGE[] = {
   "usage: ft_ssl command [flags] [file/string]\n",
-  "ft_ssl: Error:'foobar' is an invalid command.\n\nCommands:\nmd5\nsha256\n\nFlags:\n-p -q -r -s\n"
+  "ft_ssl: Error: '%s' is an invalid command.\n\nCommands:\nmd5\nsha256\n\nFlags:\n-p -q -r -s\n"
 };
 
-void print_err(t_error_code code) {
-  write(STDERR_FILENO, ERROR_MESSAGE[code], strlen(ERROR_MESSAGE[code]));
+void print_err(t_error_code code, char *detail) {
+  if (code == ERR_NO_ARGUMENTS) {
+    write(STDERR_FILENO, ERROR_MESSAGE[code], strlen(ERROR_MESSAGE[code]));
+  } else {
+    size_t size = DEFAULT_MSG_LEN + strlen(detail);
+    char buffer[size];
+    snprintf(buffer, size, ERROR_MESSAGE[code], detail);
+    write(STDERR_FILENO, buffer, strlen(buffer));
+  }
 }
 
 #endif
